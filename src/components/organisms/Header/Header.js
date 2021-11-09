af932bb51a48976a6669c52893322b668f3f7b95
Laura <laura@HYD-Admins-MacBook-Pro.local>
2021-11-09 12:32:39 +0100

    adds header repsonsive navigation

--- src/components/organisms/Header/Header.js
+++ src/components/organisms/Header/Header.js
@@ -1,17 +1,31 @@
-import React from "react"
+import React, { useState } from "react"
 import "./Header.module.scss"
 import HorizontalNav from "../../molecules/HorizontalNav/HorizontalNav"
 import Link from "../../atoms/Link/Link"
+import MenuIcon from "../../atoms/MenuIcon/MenuIcon"
+import * as style from "./Header.module.scss"
 
 const Header = () => {
+  const [showNav, setShowNav] = useState(false)
+  const toggleNav = e => {
+    e.preventDefault()
+    setShowNav(prev => !prev)
+  }
   return (
     <header>
-      <h1>Dev blog</h1>
-      <HorizontalNav>
-        <Link to={`blog`} text="blog" />
-        <Link to={`contact`} text="contact" />
-        <Link to={`/`} text="home" />
-      </HorizontalNav>
+      <div className={style.logoMenuWrap}>
+        <h1>Dev blog</h1>
+        <button className={style.menuBtn} onClick={toggleNav}>
+          <MenuIcon />
+        </button>
+      </div>
+      {showNav && (
+        <HorizontalNav>
+          <Link to={`/`} text="home" />
+          <Link to={`blog`} text="blog" />
+          <Link to={`contact`} text="contact" />
+        </HorizontalNav>
+      )}
     </header>
   )
 }
